793250543584bfb3e4decd051906dae77ba9661d
Manuel Quiñones <manuq@laptop.org>
2013-09-26 14:41:36 -0300

    webfaction middleware

--- __init__.py
+++ __init__.py
@@ -7,6 +7,16 @@ PRODUCCION = False
 
 app = Flask(__name__)
 
+class WebFactionMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = '/no-al-dakar'
+        return self.app(environ, start_response)
+
+if PRODUCCION:
+    app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
+
 # FIXME PROD cambiar
 # >>> import os
 # >>> os.urandom(24)
