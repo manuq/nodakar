55eeded432160f7157543315de640b9f38702fc4
Manuel Quiñones <manuq@laptop.org>
2013-10-04 22:43:35 -0300

    base de datos

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
+import sqlite3
+
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import g
+
+BASE_DE_DATOS = os.path.join(os.path.dirname(__file__), 'nodakar.db')
 
 PRODUCCION = False
 
@@ -25,6 +30,28 @@ if PRODUCCION:
 # >>> os.urandom(24)
 app.secret_key = "unodostres"
 
+# FIXME PROD generar
+# >>> from __init__ import init_db
+# >>> init_db()
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+            db.commit()
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(BASE_DE_DATOS)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -35,8 +62,17 @@ def gracias():
 
 @app.route('/publicar', methods=["POST"])
 def publicar():
-    print request.form['nombre'], request.form['correo'], request.form['ciudad'], request.form['provincia'], request.form['pais'], request.form['imagen']
-    print request.files
+    # FIXME guardar imagen en disco
+    # request.form['imagen']
+
+    datos = (None, request.form['nombre'], request.form['correo'],
+             request.form['ciudad'], request.form['provincia'],
+             request.form['pais'], "/media/asd")
+
+    cur = get_db().cursor()
+    cur.execute('insert into nodakar values (?,?,?,?,?,?,?)', datos)
+    get_db().commit()
+
     return redirect(url_for("gracias"))
 
 if __name__ == '__main__':
---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE nodakar (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre,
+    correo,
+    ciudad,
+    provincia,
+    pais,
+    url_imagen);
